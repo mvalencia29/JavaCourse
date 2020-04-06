af1447cdcbf5c98b580aab7ddc3758b61906e281
Miguel <miguevalencia29@hotmail.com>
2020-04-05 23:21:25 -0500

    Primer Commit, Iniciando Curso

---
+++ PrimerosPasos/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+src/Variables.java,3\1\316c7853c633be1e6b921b09f98c5afd55a56f09
---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public  static void main(String[] args){
+        String miNombre = "Miguel";
+
+        System.out.println(miNombre);
+
+        System.out.println(miNombre.length());
+
+        System.out.println(miNombre.charAt(miNombre.length() -1));
+    }
+}
