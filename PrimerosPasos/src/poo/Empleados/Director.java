775f42b8804e66401c8e049641a866ee3d361ace
Miguel <miguevalencia29@hotmail.com>
2020-04-10 01:18:22 -0500

    Aplicando refundicion para Objeto de Tipo Jefes

---
+++ PrimerosPasos/src/poo/Empleados/Director.java
@@ -0,0 +1,8 @@
+package poo.Empleados;
+
+public class Director extends Jefes {
+
+    public Director(String nombreEmpleado, double sueldo, int year, int mes, int dia) {
+        super(nombreEmpleado, sueldo, year, mes, dia);
+    }
+}
--- PrimerosPasos/src/poo/Empleados/UsoEmpleado.java
+++ PrimerosPasos/src/poo/Empleados/UsoEmpleado.java
@@ -4,15 +4,14 @@ import javax.swing.*;
 
 public class UsoEmpleado {
     public static void main(String[] args) {
-        Jefes jefeIT = new Jefes("Miguel", 55000, 2018, 10, 17);
-
         Empleado[] empleados = new Empleado[2];
-
-        empleados[0] = jefeIT;
+        empleados[0] = new Jefes("Miguel", 55000, 2018, 10, 17);
         empleados[1] = new Empleado("Miguel", 55000, 2018, 10, 17);
+        Jefes jefa = (Jefes)empleados[0];
+        jefa.setIncentivo(450000);
 
-        jefeIT.setIncentivo(1000);
-        System.out.println(empleados[0].getSueldo());
-        System.out.println(empleados[1].getSueldo());
+        for(Empleado e : empleados){
+            System.out.println(e.getSueldo());
+        }
     }
 }
